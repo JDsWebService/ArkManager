d33ccf972032c84265702e39d0ee11485100b86d
Jonathan Robinson <jdswebservice@gmail.com>
2020-10-13 00:45:18 -0400

    CSS Navbar Hotfix
    Changed CSS in the navbar to get rid of blue color & text-decoration upon hovering

--- resources/views/partials/app/head.blade.php
+++ resources/views/partials/app/head.blade.php
@@ -1,9 +1,12 @@
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<title>Tempo Bootstrap Template - Index</title>
+<title>ArkManager - @yield('title')</title>
 <meta content="" name="description">
 <meta content="" name="keywords">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Favicons -->
 <link href="{{ asset('favicon.ico') }}" rel="icon">
