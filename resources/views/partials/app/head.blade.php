491aa8b0530d618714d0a3619e82b4cf8f202a32
Jonathan Robinson <jdswebservice@gmail.com>
2020-12-08 13:52:02 -0500

    Added Meta Tags

--- resources/views/partials/app/head.blade.php
+++ resources/views/partials/app/head.blade.php
@@ -1,9 +1,7 @@
-<meta charset="utf-8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<!-- Meta Tags -->
+@include('partials.metatags')
 
 <title>ArkManager.app - @yield('title')</title>
-<meta content="" name="description">
-<meta content="" name="keywords">
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
---
+++ resources/views/partials/metatags.blade.php
@@ -0,0 +1,9 @@
+<meta name="title" content="ArkManager - Full Suite Of Tools For Your Ark Adventure">
+<meta name="description" content="ArkManager is a full suite of tools to enable survivors like yourself to take full advantage of the game! Dino Tracker, Official Server Trade Hub, ...">
+<meta name="keywords" content="ark survival evolved, ark survival evolved wiki, ark survival evolved ps4, ark dino tracker, ark breeding, ark breeding calculator, ark smart breeding, ark breeding guide">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="revisit-after" content="1 days">
+<meta name="author" content="JD's Web Service">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
