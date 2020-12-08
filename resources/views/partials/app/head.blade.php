53d58437d1b3b55750194769f73869d4f2c8f3fa
Jonathan Robinson <jdswebservice@gmail.com>
2020-12-08 12:33:27 -0500

    Added Favicons
    Added favicons for all modern web browsers and apple devices, both current gen and last gen

--- resources/views/partials/app/head.blade.php
+++ resources/views/partials/app/head.blade.php
@@ -8,8 +8,27 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<!-- Favicons -->
-<link href="{{ asset('favicon.ico') }}" rel="icon">
+<!-- Favicons (Desktop Broswers) -->
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon/arkmanager-favicon-48x48.png') }}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/arkmanager-favicon-96x96.png') }}">
+<link rel="icon" type="image/png" sizes="144x144" href="{{ asset('favicon/arkmanager-favicon-144x144.png') }}">
+<link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon/arkmanager-favicon-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="256x256" href="{{ asset('favicon/arkmanager-favicon-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="384x384" href="{{ asset('favicon/arkmanager-favicon-384x384.png') }}">
+<link rel="icon" type="image/png" sizes="512x512" href="{{ asset('favicon/arkmanager-favicon-512x512.png') }}">
+<!-- Favicons (Apple Current Gen) -->
+<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicon/arkmanager-apple-touch-icon-120x120.png') }}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicon/arkmanager-apple-touch-icon-152x152.png') }}">
+<link rel="apple-touch-icon" sizes="167x167" href="{{ asset('favicon/arkmanager-apple-touch-icon-167x167.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/arkmanager-apple-touch-icon-180x180.png') }}">
+<!-- Favicons (Apple Previous Gen) -->
+<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicon/arkmanager-apple-touch-icon-57x57.png') }}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicon/arkmanager-apple-touch-icon-60x60.png') }}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicon/arkmanager-apple-touch-icon-72x72.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicon/arkmanager-apple-touch-icon-76x76.png') }}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicon/arkmanager-apple-touch-icon-114x114.png') }}">
+<!-- Favicon (Safari) -->
+<link rel="mask-icon" href="{{ asset('favicon/favicon.svg') }}" color="#ff0000">
 
 <!-- Font Awesome -->
 <script src="https://kit.fontawesome.com/0d9c5a4db2.js" crossorigin="anonymous"></script>
